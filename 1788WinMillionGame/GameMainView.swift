425ded8e12120fea1b9f27429766a799b7c7f1c5
NikNik <nkru@narod.ru>
2022-02-26 22:01:50 +0300

    add stack with buttons

--- 1788WinMillionGame/GameMainView.swift
+++ 1788WinMillionGame/GameMainView.swift
@@ -12,13 +12,23 @@ final class GameMainView: UIView {
     var questionLabel: UILabel = {
         let label = UILabel()
         label.text                      = "Список вопросов"
-        label.font                      = UIFont.boldSystemFont(ofSize: 30)
+        label.font                      = UIFont.systemFont(ofSize: 30)
         label.textAlignment             = .center
         label.numberOfLines             = 0
         label.adjustsFontSizeToFitWidth = true
         label.textColor                 = .white
-        label.backgroundColor           = .systemRed
+        label.backgroundColor           = .gray
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
         return label
+    }()
+    
+    var stackView: UIStackView = {
+        let stackView          = UIStackView()
+        stackView.axis         = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing      = 10
+        return stackView
     }()
     
 //    MARK: - LifeCycle
@@ -36,22 +46,42 @@ final class GameMainView: UIView {
     private func setupViews() {
     
         addSubview(questionLabel)
+        addSubview(stackView)
+        addButtonToCtackView()
     }
     
     private func setupConstraints() {
+        
         setQuestionLabelConstraints()
+        setStackViewConstraints()
     }
     
-    private func configqustionLabel() {
- 
+    private func addButtonToCtackView() {
+        let numberOfButtons = 5
+        for i in 1...numberOfButtons {
+            let button = ActionButton()
+            button.setTitle("\(i)", for: .normal)
+            stackView.addArrangedSubview(button)
+        }
     }
     
     private func setQuestionLabelConstraints() {
         questionLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            questionLabel.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor, constant: 80),
+            questionLabel.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 80),
             questionLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
-            questionLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -40)
+            questionLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -40),
         ])
     }
+    
+    private func setStackViewConstraints() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: self.questionLabel.bottomAnchor, constant: 30),
+            stackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -40),
+            stackView.bottomAnchor.constraint(equalTo: self.layoutMarginsGuide.bottomAnchor, constant: -40)
+        ])
+    }
+    
 }
