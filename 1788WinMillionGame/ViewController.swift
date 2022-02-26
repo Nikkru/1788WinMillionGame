36a409a2572944e43b7a972969f80a5923a7865f
NikNik <nkru@narod.ru>
2022-02-26 17:11:57 +0300

    Initial Commit

---
+++ 1788WinMillionGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1788WinMillionGame
+//
+//  Created by Nikolai Krusser on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
