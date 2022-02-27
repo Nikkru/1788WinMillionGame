74829a700e38fcb58eee615e5301bcf271c94277
NikNik <nkru@narod.ru>
2022-02-27 14:49:51 +0300

    add one button

---
+++ 1788WinMillionGame/ActionButton.swift
@@ -0,0 +1,27 @@
+//
+//  AnswerButton.swift
+//  1788WinMillionGame
+//
+//  Created by Nikolai Krusser on 26.02.2022.
+//
+
+import UIKit
+
+final class ActionButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupButton() {
+        setTitleColor((.white), for: .normal)
+        backgroundColor = .darkGray
+        titleLabel?.font = UIFont(name: "answer to guestion", size: 100)
+        layer.cornerRadius = 10
+    }
+}
--- 1788WinMillionGame/GameMainView.swift
+++ 1788WinMillionGame/GameMainView.swift
@@ -12,13 +12,35 @@ final class GameMainView: UIView {
     var questionLabel: UILabel = {
         let label = UILabel()
         label.text                      = "Список вопросов"
-        label.font                      = UIFont.boldSystemFont(ofSize: 30)
+        label.font                      = UIFont.systemFont(ofSize: 30)
         label.textAlignment             = .center
         label.numberOfLines             = 0
         label.adjustsFontSizeToFitWidth = true
         label.textColor                 = .white
-        label.backgroundColor           = .systemRed
+        label.backgroundColor           = .systemGray
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
         return label
+    }()
+    
+    var scoreLabel: UILabel = {
+        let label = UILabel()
+        label.text                      = "Вы выиграли 0 рублей"
+        label.font                      = UIFont.systemFont(ofSize: 28)
+        label.textAlignment             = .center
+        label.numberOfLines             = 0
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor                 = .white
+        label.backgroundColor           = .systemGray2
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    var firstButton: ActionButton = {
+        let button = ActionButton()
+        button.setTitle("Вопрос на миллион", for: .normal)
+        return button
     }()
     
 //    MARK: - LifeCycle
@@ -36,22 +58,44 @@ final class GameMainView: UIView {
     private func setupViews() {
     
         addSubview(questionLabel)
+        addSubview(scoreLabel)
+        addSubview(firstButton)
     }
     
     private func setupConstraints() {
         setQuestionLabelConstraints()
+        setScoreLabelConstraints()
+        setFirstButtonConstraints()
     }
     
-    private func configqustionLabel() {
- 
+    private func setScoreLabelConstraints() {
+        
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor, constant: 40),
+            scoreLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
+            scoreLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -40)
+        ])
     }
     
     private func setQuestionLabelConstraints() {
+        
         questionLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            questionLabel.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor, constant: 80),
+            questionLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 20),
             questionLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
             questionLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -40)
         ])
     }
+    
+    private func setFirstButtonConstraints() {
+        
+        firstButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            firstButton.topAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 20),
+            firstButton.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 40),
+            firstButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -40)
+        ])
+    }
+
 }
