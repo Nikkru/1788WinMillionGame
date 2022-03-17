cefcec425cdc3f5ce1882a2b11fcd6b61f69c452
NikNik <nkru@narod.ru>
2022-03-17 20:14:08 +0300

    delete storyboard

--- 1788WinMillionGame/App/SceneDelegate.swift
+++ 1788WinMillionGame/App/SceneDelegate.swift
@@ -15,11 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window?.windowScene = windowScene
+        window = UIWindow(windowScene: windowScene)
+//        let controller = StartViewController()
+//        let navigationController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = StartViewController()
         window?.makeKeyAndVisible()
-        let controller = StartViewController()
-        let navigationController = UINavigationController(rootViewController: controller)
-        window?.rootViewController = navigationController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
